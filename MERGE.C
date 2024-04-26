646931ff6b4aa0b057aab622fd734d2a771f6e53
Sneha <107575680+snehamavinakai@users.noreply.github.com>
2024-04-27 00:02:23 +0530

    Update MERGE.C

--- MERGE.C
+++ MERGE.C
@@ -2,37 +2,37 @@
 #include<conio.h>
 #include<stdlib.h>
 void mergesort(int[],int,int);
-void merge(int[],int,int,int,int);
-void mergesort(int a[100],int i,int j)
+void merge(int[],int,int,int);
+void mergesort(int a[100],int l,int h)
 {
 int mid;
-if(i<j)
+if(l<h)
 {
-mid=(i+j)/2;
-mergesort(a,i,mid);
-mergesort(a,mid+1,j);
-merge(a,i,mid,mid+1,j);
+mid=(l+h)/2;
+mergesort(a,l,mid);
+mergesort(a,mid+1,h);
+merge(a,l,mid,h);
 }
 }
-void merge(int a[100],int i1,int j1,int i2,int j2)
+void merge(int a[],int l,int mid,int h)
 {
 int temp[100];
 int i,j,k;
-i=i1;
-j=i2;
+i=l;
+j=mid+1;
 k=0;
-while(i<=j1&&j<=j2)
+while(i<=mid&&j<=h)
 {
 if(a[i]<a[j])
 temp[k++]=a[i++];
 else
 temp[k++]=a[j++];
 }
-while(i<=j1)
+while(i<=mid)
 temp[k++]=a[i++];
-while(j<=j2)
+while(j<=h)
 temp[k++]=a[j++];
-for(i=i1,j=0;i<=j2;i++,j++)
+for(i=l,j=0;i<=h;i++,j++)
 a[i]=temp[j];
 }
 void main()
